4f949052444832766da6265da14de5a84e32f131
adolfogarlez <adolfogarlez@gmail.com>
2020-02-13 18:46:58 +0800

    Initial commit

---
+++ src/main/java/guru/springframework/sfgrecipe/controllers/MainController.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgrecipe.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    String getIndex(Model model) {
+        return "index";
+    }
+}
